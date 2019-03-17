eae0fc7ebd643176bbea1d38c75f85476c378c27
Per-Øyvind Bruun <perri@thesource.no>
2019-03-17 22:36:29 +0100

    Updated for pip

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup
 setup(
   name = 'sectoralarmlib',
   packages = ['sectoralarmlib'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.3',
   description = 'Library for Sector Alarm',
   author = 'Per-Øyvind Bruun',
   author_email = 'per-oyvind.bruun@vitari.no',
@@ -10,4 +11,7 @@ setup(
   download_url = 'https://github.com/peroyvind/sectoralarmlib/tarball/0.1',
   keywords = ['sectoralarm'], 
   classifiers = [],
+  install_requires=[
+          'requests'
+  ]
 )
