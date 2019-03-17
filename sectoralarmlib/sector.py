4fd3dbca19655ed9f3a8610851c8e07da87a1886
Per-Øyvind Bruun <perri@thesource.no>
2019-03-17 23:59:05 +0100

    Added arm/disarm commands

--- sectoralarmlib/sector.py
+++ sectoralarmlib/sector.py
@@ -8,9 +8,11 @@ req = requests.session()
 
 class SectorAlarm:
 
-    def __init__(self, username, password):
+    def __init__(self, username, password, siteid, panelcode):
         self.username = username
         self.password = password
+        self.code = panelcode
+        self.siteid = siteid
 
     def login(self):
 
@@ -28,24 +30,74 @@ class SectorAlarm:
             "User-Agent" : "Safari/537.36"
         }
 
-        #r = requests.session()
-        r2 = req.post(baseUrl + '/User/Login?ReturnUrl=%2f', data=formdata, headers=headers)
+        response = req.post(baseUrl + '/User/Login?ReturnUrl=%2f', data=formdata, headers=headers)
         
-        if "<title>Login</title>" in r2.text:
+        if "<title>Login</title>" in response.text:
             raise Exception("Wrong username or password")
-        elif "<title>Sector Alarm</title>" in r2.text:
+        elif "<title>Sector Alarm</title>" in response.text:
             return "ok"
 
     def alarmstatus(self):
         self.login()
 
-        r2 = req.post(baseUrl + '/Panel/GetOverview/')
+        response = req.post(baseUrl + '/Panel/GetOverview/')
 
-        svar = json.loads(r2.text)
+        svar = json.loads(response.text)
 
 
         if svar['Panel']['ArmedStatus'] == 'disarmed':
             return "OFF"
-        elif svar['Panel']['ArmedStatus'] == 'armed':
+        else
             return "ON"
 
+    def arm(self):
+        self.login()
+
+        payload = { "ArmCmd":"Total", "PanelCode":self.code, "HasLocks":"false", "id":self.siteid }
+
+        headers = {
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "en-US,en;q=0.9,sv;q=0.8",
+            "Cache-Control": "max-age=0",
+            "Connection": "keep-alive",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Content-Length": str(sys.getsizeof(payload)),
+            "Upgrade-Insecure-Requests" : '1',
+            "User-Agent" : "Safari/537.36"
+        }
+
+        response = req.post(baseUrl + '/Panel/ArmPanel/', data=payload, headers=headers)
+
+        svar = json.loads(response.text)
+
+        if svar["Status"] != "success":
+            raise Exception("Something went wrong while arming the alarm.")
+        else
+            return "Armed"
+
+    def disarm(self):
+        self.login()
+
+        payload = { "ArmCmd":"Disarm", "PanelCode":self.code, "HasLocks":"false", "id":self.siteid }
+
+        headers = {
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "en-US,en;q=0.9,sv;q=0.8",
+            "Cache-Control": "max-age=0",
+            "Connection": "keep-alive",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Content-Length": str(sys.getsizeof(payload)),
+            "Upgrade-Insecure-Requests" : '1',
+            "User-Agent" : "Safari/537.36"
+        }
+
+        response = req.post(baseUrl + '/Panel/ArmPanel/', data=payload, headers=headers)
+        
+        svar = json.loads(response.text)
+
+        if svar["Status"] != "success":
+            raise Exception("Something went wrong while disarming the alarm.")
+        else
+            return "Disarmed"
--- setup.py
+++ setup.py
@@ -3,15 +3,16 @@ from setuptools import setup
 setup(
   name = 'sectoralarmlib',
   packages = ['sectoralarmlib'], # this must be the same as the name above
-  version = '0.3',
+  version = '0.1',
   description = 'Library for Sector Alarm',
   author = 'Per-Øyvind Bruun',
   author_email = 'per-oyvind.bruun@vitari.no',
   url = 'https://github.com/peroyvind/sectoralarmlib',
   download_url = 'https://github.com/peroyvind/sectoralarmlib/tarball/0.1',
   keywords = ['sectoralarm'], 
-  classifiers = [],
+  classifiers = [ 'Programming Language :: Python :: 3.6' ],
   install_requires=[
           'requests'
-  ]
+  ],
+  
 )
