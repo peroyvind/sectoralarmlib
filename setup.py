47ae0079942f498d9003a39916b4ca65404e3959
Per-Øyvind Bruun <perri@thesource.no>
2019-03-20 23:03:06 +0100

    Bumped to 0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
   name = 'sectoralarmlib',
   packages = ['sectoralarmlib'], # this must be the same as the name above
-  version = '0.2',
+  version = '0.3',
   description = 'Library for Sector Alarm',
   author = 'Per-Øyvind Bruun',
   author_email = 'per-oyvind.bruun@vitari.no',
