0a9629bedffaedf57ee01af1ed951a2567266c77
Per-Øyvind Bruun <per-oyvind.bruun@vitari.no>
2019-10-13 16:51:27 +0200

    Bumped to 0.8

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
   name = 'sectoralarmlib',
   packages = ['sectoralarmlib'], # this must be the same as the name above
-  version = '0.7',
+  version = '0.8',
   description = 'Library for Sector Alarm',
   author = 'Per-Øyvind Bruun',
   author_email = 'per-oyvind.bruun@vitari.no',
   url = 'https://github.com/peroyvind/sectoralarmlib',
   keywords = ['sectoralarm'], 
-  classifiers = [ 'Programming Language :: Python :: 3.6', 'Programming Language :: Python :: 3.5'  ],
+  classifiers = [ 'Programming Language :: Python :: 3.7', 'Programming Language :: Python :: 3.6', 'Programming Language :: Python :: 3.5'  ],
   install_requires=[
           'requests'
   ],
