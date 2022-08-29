a019ea577bdfb94875839d05aa8d0de54947b197
Hannes Landeholm <landeholm@gmail.com>
2022-08-29 18:56:51 +0200

    Rate limiting + various bug fixes and improvements.
    Using this gave me authorization error after a while, presumably due to rate limiting from generating a large number of sessions.
    
     - Fix bug where it logs in to a new session for every call.
     - Fix bug in regex version parsing.
     - Fix bug in url encoding payload for login (not escaping special chars like +).
     - Crash early with raise_for_status() if requests fail and get debuggable error.
     - Use a common user agent and allow it to be configurable.
     - Clean up headers. Remove not needed.
     - Remove globals and move all state into class.

--- sectoralarmlib/sector.py
+++ sectoralarmlib/sector.py
@@ -1,12 +1,11 @@
+import time
 import requests
 import json
-import sys
 import re
+import urllib.parse
 
+base_url = 'https://mypagesapi.sectoralarm.net'
 
-baseUrl = "https://mypagesapi.sectoralarm.net"
-req = requests.session()
-version = ''
 
 class SectorAlarm:
 
@@ -15,163 +14,154 @@ class SectorAlarm:
         self.password = password
         self.code = panelcode
         self.siteid = siteid
+        self.user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
+
+        # After logging in, wait this many seconds to avoid being subsequently rate-limited.
+        self.login_cooldown_sec = 5
+
+        self.session = requests.session()
+        self.version = None
+        self.logged_in = False
 
     def Login(self):
+        if self.logged_in:
+            return
 
-        formdata = 'userID=' + self.username + '&password=' + self.password
+        params = {
+            'userID': self.username,
+            'password': self.password,
+        }
+        formdata = urllib.parse.urlencode(params)
 
         headers = {
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-            "Accept-Encoding": "gzip, deflate, br",
-            "Accept-Language": "en-US,en;q=0.9,sv;q=0.8",
-            "Cache-Control": "max-age=0",
-            "Connection": "keep-alive",
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Content-Length": str(sys.getsizeof(formdata)),
-            "Upgrade-Insecure-Requests" : '1',
-            "User-Agent" : "Safari/537.36"
+            'Cache-Control': 'max-age=0',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'User-Agent': self.user_agent,
         }
 
-        response = req.post(baseUrl + '/User/Login?ReturnUrl=%2f', data=formdata, headers=headers)
-        
-        if "<title>Login</title>" in response.text:
-            raise RuntimeError("Wrong username or password")
-        elif "<title>Sector Alarm</title>" in response.text:
-            x = re.findall("Scripts\/main.js\?(v.+)\"", response.text)
-            global version
-            version = x[0].split('\"',1)[0]
-            return "ok"
-            
+        response = self.session.post(base_url + '/User/Login?ReturnUrl=%2f', data=formdata, headers=headers)
+        response.raise_for_status()
+
+        if '<title>Login</title>' in response.text:
+            raise RuntimeError('Wrong username or password')
+
+        if not '<title>Sector Alarm</title>' in response.text:
+            raise RuntimeError('Login Failed')
+
+        x = re.findall("Scripts\/main.js\?(v[^\"&]+)\"", response.text)
+        self.version = x[0].split('\'', 1)[0]
+        self.logged_in = True
+
+        time.sleep(self.login_cooldown_sec)
+
     def AlarmStatus(self):
         self.Login()
 
-        payload = { "id": self.siteid, "Version":version  }
+        payload = {'id': self.siteid, 'Version': self.version}
 
         headers = {
-            'Accept': 'application/json, text/plain, */*',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Accept-Language': 'en-US,en;q=0.9,sv;q=0.8',
+            'Accept': 'application/json',
             'Cache-Control': 'max-age=0',
             'Content-Type': 'application/json;charset=UTF-8',
-            "Content-Length": str(sys.getsizeof(payload)),
-            'Connection': 'keep-alive',
-            'User-Agent' : 'Safari/537.36'
+            'User-Agent': self.user_agent,
         }
 
-        response = req.post(baseUrl + '/Panel/GetOverview/', data=json.dumps(payload), headers=headers)
+        response = self.session.post(base_url + '/Panel/GetOverview/', data=json.dumps(payload), headers=headers)
+        response.raise_for_status()
 
-        svar = json.loads(response.text)    
-
+        svar = json.loads(response.text)
 
         if svar['Panel']['ArmedStatus'] == 'disarmed':
-            return "OFF"
+            return 'OFF'
         elif svar['Panel']['ArmedStatus'] == 'partialarmed':
-            return "PARTIAL"
+            return 'PARTIAL'
         else:
-            return "ON"
+            return 'ON'
 
     def Arm(self):
         self.Login()
 
-        payload = { "ArmCmd":"Total", "PanelCode":self.code, "HasLocks":"false", "id":self.siteid }
+        payload = {'ArmCmd': 'Total', 'PanelCode': self.code, 'HasLocks': 'false', 'id': self.siteid}
 
         headers = {
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-            "Accept-Encoding": "gzip, deflate, br",
-            "Accept-Language": "en-US,en;q=0.9,sv;q=0.8",
-            "Cache-Control": "max-age=0",
-            "Connection": "keep-alive",
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Content-Length": str(sys.getsizeof(payload)),
-            "Upgrade-Insecure-Requests" : '1',
-            "User-Agent" : "Safari/537.36"
+            'Accept': 'application/json',
+            'Cache-Control': 'max-age=0',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'User-Agent': self.user_agent,
         }
 
-        response = req.post(baseUrl + '/Panel/ArmPanel/', data=payload, headers=headers)
+        response = self.session.post(base_url + '/Panel/ArmPanel/', data=payload, headers=headers)
+        response.raise_for_status()
 
         svar = json.loads(response.text)
 
-        if svar["status"] != "success":
-            raise RuntimeError("Something went wrong while arming the alarm.")
+        if svar['status'] != 'success':
+            raise RuntimeError('Something went wrong while arming the alarm.')
         else:
-            return "Armed"
+            return 'Armed'
 
     def Disarm(self):
         self.Login()
 
-        payload = { "ArmCmd":"Disarm", "PanelCode":self.code, "HasLocks":"false", "id":self.siteid }
+        payload = {'ArmCmd': 'Disarm', 'PanelCode': self.code, 'HasLocks': 'false', 'id': self.siteid}
 
         headers = {
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-            "Accept-Encoding": "gzip, deflate, br",
-            "Accept-Language": "en-US,en;q=0.9,sv;q=0.8",
-            "Cache-Control": "max-age=0",
-            "Connection": "keep-alive",
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Content-Length": str(sys.getsizeof(payload)),
-            "Upgrade-Insecure-Requests" : '1',
-            "User-Agent" : "Safari/537.36"
+            'Accept': 'application/json',
+            'Cache-Control': 'max-age=0',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'User-Agent': self.user_agent,
         }
 
-        response = req.post(baseUrl + '/Panel/ArmPanel/', data=payload, headers=headers)
-        
+        response = self.session.post(base_url + '/Panel/ArmPanel/', data=payload, headers=headers)
+        response.raise_for_status()
+
         svar = json.loads(response.text)
 
-        if svar["status"] != "success":
-            raise RuntimeError("Something went wrong while disarming the alarm.")
+        if svar['status'] != 'success':
+            raise RuntimeError('Something went wrong while disarming the alarm.')
         else:
-            return "Disarmed"
+            return 'Disarmed'
 
     def ArmPartial(self):
         self.Login()
 
-        payload = { "ArmCmd":"Partial", "PanelCode":self.code, "HasLocks":"false", "id":self.siteid }
+        payload = {'ArmCmd': 'Partial', 'PanelCode': self.code, 'HasLocks': 'false', 'id': self.siteid}
 
         headers = {
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-            "Accept-Encoding": "gzip, deflate, br",
-            "Accept-Language": "en-US,en;q=0.9,sv;q=0.8",
-            "Cache-Control": "max-age=0",
-            "Connection": "keep-alive",
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Content-Length": str(sys.getsizeof(payload)),
-            "Upgrade-Insecure-Requests" : '1',
-            "User-Agent" : "Safari/537.36"
+            'Accept': 'application/json',
+            'Cache-Control': 'max-age=0',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'User-Agent': self.user_agent,
         }
 
-        response = req.post(baseUrl + '/Panel/ArmPanel/', data=payload, headers=headers)
-        
+        response = self.session.post(base_url + '/Panel/ArmPanel/', data=payload, headers=headers)
+        response.raise_for_status()
+
         svar = json.loads(response.text)
 
-        if svar["status"] != "success":
-            raise RuntimeError("Something went wrong while partial arming the alarm.")
+        if svar['status'] != 'success':
+            raise RuntimeError('Something went wrong while partial arming the alarm.')
         else:
-            return "PartialArmed"
-
+            return 'PartialArmed'
 
     def GetTemps(self):
-        temps = []
-
         self.Login()
 
-        payload = { "id": self.siteid, "Version":version  }
+        temps = []
+
+        payload = {'id': self.siteid, 'Version': self.version}
 
         headers = {
-            'Accept': 'application/json, text/plain, */*',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Accept-Language': 'en-US,en;q=0.9,sv;q=0.8',
+            'Accept': 'application/json',
             'Cache-Control': 'max-age=0',
             'Content-Type': 'application/json;charset=UTF-8',
-            "Content-Length": str(sys.getsizeof(payload)),
-            'Connection': 'keep-alive',
-            'User-Agent' : 'Safari/537.36'
+            'User-Agent': self.user_agent,
         }
 
-        response = req.post(baseUrl + '/Panel/GetTempratures/', data=json.dumps(payload), headers=headers)
+        response = self.session.post(base_url + '/Panel/GetTempratures/', data=json.dumps(payload), headers=headers)
+        response.raise_for_status()
 
         for temp in json.loads(response.text):
-            temps.append((temp["Label"], temp["Temprature"]))
-
+            temps.append((temp['Label'], temp['Temprature']))
 
         return temps
-        
